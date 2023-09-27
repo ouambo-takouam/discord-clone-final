3caba189040866d0749f12e27279ee18038fde05
Ouambo Patrick <ouambotakouam@gmail.com>
2023-09-27 12:45:23 +0200

    clerk authentication added

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,3 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <p>Protected routes</p>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
