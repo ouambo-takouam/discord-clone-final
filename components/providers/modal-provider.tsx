937232fa5cd8b67ce6fdd0b26628481adca67d76
Ouambo Patrick <ouambotakouam@gmail.com>
2023-09-28 13:50:25 +0200

    modal provider added

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -6,11 +6,15 @@ import CreateServerModal from "@components/modals/create-server-modal";
 export default function ModalProvider() {
   const [mounted, setMounted] = useState(false);
 
-  if (!mounted) return null;
-
   useEffect(() => {
     setMounted(true);
   }, []);
 
-  return <CreateServerModal />;
+  if (!mounted) return null;
+
+  return (
+    <>
+      <CreateServerModal />
+    </>
+  );
 }
