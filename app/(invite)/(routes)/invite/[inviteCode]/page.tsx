f7cf56769cbe40aa33dffe6e0d7b23e12aed8e4f
Ouambo Patrick <ouambotakouam@gmail.com>
2023-09-29 17:04:30 +0200

    invite page added

---
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -0,0 +1,52 @@
+import { redirectToSignIn } from "@clerk/nextjs";
+
+import { db } from "@lib/db";
+import { currentProfile } from "@lib/current-profile";
+
+import { redirect } from "next/navigation";
+
+interface InviteCodePageProps {
+  params: {
+    inviteCode: string;
+  };
+}
+
+export default async function InviteCodePage({ params }: InviteCodePageProps) {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  if (!params.inviteCode) {
+    return redirect("/");
+  }
+
+  const existingServer = await db.server.findFirst({
+    where: {
+      inviteCode: params.inviteCode,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (existingServer) {
+    return redirect(`/servers/${existingServer.id}`);
+  }
+
+  const server = await db.server.update({
+    where: {
+      inviteCode: params.inviteCode,
+    },
+    data: {
+      members: {
+        create: [{ profileId: profile.id }],
+      },
+    },
+  });
+
+  return <div>Hello invite</div>;
+}
